a6da0658d0dd71318ed8f783b752e91f44d5d7ea
Alvin <alvinmutisya@gmail.com>
2016-11-13 13:11:24 +0300

    Modified to conform to python function naming conventions

--- http_and_web_test.py
+++ http_and_web_test.py
@@ -5,18 +5,34 @@ import http_and_web
 class HttpandWebTestCase(unittest.TestCase):
 	#Test against integer inputs
 	def test_artist_name_is_int(self):
-		self.assertEqual(http_and_web.get_Name(20), "Operation cannot allow " + str(type(20)) + ", artist_name is a string")
+		int_test = http_and_web.get_name(20)
+		self.assertEqual(int_test, "Operation cannot allow " + 
+							str(type(20)) + ", artist_name is a string")
 	#Test against various data structures
 	def test_artist_name_is_dictionary(self):
-		self.assertEqual(http_and_web.get_Name({}), "Operation cannot allow " + str(type({})) + ", artist_name is a string")
+		dict_test = http_and_web.get_name({})
+		self.assertEqual(dict_test, "Operation cannot allow " +
+							str(type({})) + ", artist_name is a string")
 	def test_artist_name_is_list(self):
-		self.assertEqual(http_and_web.get_Name([]), "Operation cannot allow " + str(type([])) + ", artist_name is a string")
+		list_test = http_and_web.get_name([])
+		self.assertEqual(list_test, "Operation cannot allow " +
+							str(type([])) + ", artist_name is a string")
 	def test_artist_name_is_tuple(self):
-		self.assertEqual(http_and_web.get_Name(('arg1', 'arg2')), "Operation cannot allow " + str(type(("arg1", "arg2"))) + ", artist_name is a string")
+		tuple_test = http_and_web.get_name(('arg1', 'arg2'))
+		self.assertEqual(tuple_test, "Operation cannot allow " +
+							str(type(("arg1", "arg2"))) +
+										", artist_name is a string")
 	def test_artist_name_is_set(self):
-		self.assertEqual(http_and_web.get_Name({'arg1', 'arg2'}), "Operation cannot allow " + str(type({"arg1", "arg2"})) + ", artist_name is a string")
+		set_test = http_and_web.get_name({'arg1', 'arg2'})
+		self.assertEqual(set_test , "Operation cannot allow " +
+							str(type({"arg1", "arg2"})) +
+										", artist_name is a string")
 	#Test against an empty string
 	def test_artist_name_is_present(self):
-		self.assertEqual(http_and_web.get_Name(""), "artist name is a requirement")
+		test_artist_name = http_and_web.get_name("")
+		self.assertEqual(test_artist_name, "artist name is a requirement")
 	def test_correct_url(self):
-		self.assertEqual(http_and_web.get_Name("kanye West"), ("https://itunes.apple.com/search?term=kanyewest&entity=musicVideo", "kanyeWest"))
+		test_url = http_and_web.get_name("kanye West")
+		url = ("https://itunes.apple.com/search?term=kanyewest&entity=musicVideo",
+				"kanyeWest")
+		self.assertEqual(test_url, url)
